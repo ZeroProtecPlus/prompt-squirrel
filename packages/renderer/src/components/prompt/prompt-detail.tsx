e5f992f26e07216b555b47e545b8e5503db24549
r3dsd <dogsheepduck@gmail.com>
2025-06-08 14:40:53 +0900

    feat: 프롬프트 제목 변경 기능

--- packages/renderer/src/components/prompt/prompt-detail.tsx
+++ packages/renderer/src/components/prompt/prompt-detail.tsx
@@ -14,13 +14,13 @@ import {
 import { Textarea } from '@/components/ui/textarea';
 import { cn } from '@/lib/utils';
 import { usePromptStore } from '@/store';
-import { Edit, Save } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { CategoryFilterComboBox } from '../category/category-filter';
 import CopyButton from '../shared/copy-button';
-import { Button } from '../ui/button';
 import { Label } from '../ui/label';
 import { Separator } from '../ui/separator';
+import EditSaveToggle from '../shared/edit-save-toggle';
+import { Input } from '../ui/input';
 
 interface PromptDetailProps {
     prompt: Prompt | null;
@@ -30,20 +30,26 @@ interface PromptDetailProps {
 export default function PromptDetail({ prompt, onClose }: PromptDetailProps) {
     const updatePrompt = usePromptStore((state) => state.updatePrompt);
 
-    const [isEditMode, setIsEditMode] = useState<boolean>(false);
+    const [isNameEditMode, setIsNameEditMode] = useState<boolean>(false);
+    const [isPromptEditMode, setIsPromptEditMode] = useState<boolean>(false);
 
     const [category, setCategory] = useState<Category | null>(prompt?.category || null);
+    const [promptName, setPromptName] = useState<string>(prompt?.name || '');
+    const originalPromptName = useRef<string>(promptName);
     const [promptText, setPromptText] = useState<string>(prompt?.prompt || '');
-    const [originalPromptText, setOriginalPromptText] = useState<string>(promptText);
+    const originalPromptText = useRef<string>(promptText);
 
     const open = !!prompt;
 
     useEffect(() => {
-        if (prompt) {
-            setPromptText(prompt.prompt);
-            setOriginalPromptText(prompt.prompt);
-            setIsEditMode(false);
-        }
+        if (!prompt) return;
+
+        setPromptText(prompt.prompt);
+        originalPromptText.current = prompt.prompt;
+        setPromptName(prompt.name);
+        originalPromptName.current = prompt.name;
+        setIsPromptEditMode(false);
+        setIsNameEditMode(false);
     }, [prompt]);
 
     function onOpenChange(open: boolean) {
@@ -58,18 +64,40 @@ export default function PromptDetail({ prompt, onClose }: PromptDetailProps) {
         setCategory(newCategory);
     }
 
-    async function handleEditToggle() {
+    async function handlePromptSave() {
         if (!prompt) return;
+        setIsPromptEditMode(false);
+        if (promptText === originalPromptText.current) return;
 
-        if (isEditMode) {
-            setIsEditMode(false);
-            if (promptText === originalPromptText) return;
-            await updatePrompt({
-                id: prompt.id,
-                prompt: promptText,
-            });
-        } else {
-            setIsEditMode(true);
+        const updatedPrompt = await updatePrompt({
+            id: prompt.id,
+            prompt: promptText,
+        });
+        originalPromptText.current = updatedPrompt.prompt;
+    }
+
+    async function handleNameSave() {
+        if (!prompt) return;
+        setIsNameEditMode(false);
+        if (promptName === originalPromptName.current) return;
+
+        const updatedPrompt = await updatePrompt({
+            id: prompt.id,
+            name: promptName,
+        });
+        originalPromptName.current = updatedPrompt.name;
+    }
+
+    async function handleNameInputKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key === 'Enter') {
+            event.stopPropagation();
+            event.preventDefault();
+            await handleNameSave();
+        } else if (event.key === 'Escape') {
+            event.stopPropagation();
+            event.preventDefault();
+            setIsNameEditMode(false);
+            setPromptName(originalPromptName.current);
         }
     }
 
@@ -77,8 +105,29 @@ export default function PromptDetail({ prompt, onClose }: PromptDetailProps) {
         <Sheet open={open} onOpenChange={onOpenChange}>
             <SheetContent className="p-4 gap-2 md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl">
                 <div className="flex-shrink-0">
-                    <SheetHeader className="p-0 pt-4">
-                        <SheetTitle>{prompt?.name}</SheetTitle>
+                    <SheetHeader className="p-0 pt-6">
+                        <SheetTitle asChild>
+                            <div className='flex items-center justify-between gap-2'>
+                                <Input
+                                    className={cn('transition-all',
+                                        isNameEditMode
+                                        ? 'border shadow-sm focus-visible:ring-1'
+                                        : 'border-none shadow-none bg-transparent pointer-events-none focus-visible:ring-0 select-none text-ellipsis'
+                                    )}
+                                    value={promptName}
+                                    onChange={(e) => setPromptName(e.target.value)}
+                                    readOnly={!isNameEditMode}
+                                    tabIndex={isNameEditMode ? 0 : -1}
+                                    onKeyDown={handleNameInputKeyDown}
+                                />
+                                <EditSaveToggle
+                                    className="size-9 sm:size-10"
+                                    value={isNameEditMode}
+                                    onEditMode={() => setIsNameEditMode(true)}
+                                    onSaveMode={handleNameSave}
+                                />
+                            </div>
+                        </SheetTitle>
                         <SheetDescription className="sr-only" />
                         <CategoryFilterComboBox value={category} onSelect={handleCategorySelect} />
                     </SheetHeader>
@@ -88,41 +137,28 @@ export default function PromptDetail({ prompt, onClose }: PromptDetailProps) {
                 <div className="flex-1 flex flex-col space-y-2 min-h-0">
                     <div className="flex items-center justify-between">
                         <Label htmlFor="prompt-textarea">프롬프트</Label>
-                        <Button
-                            variant="outline"
-                            className="size-10 sm:size-8"
-                            onClick={handleEditToggle}
-                        >
-                            <Edit
-                                className={cn(
-                                    'size-5 absolute transition-all duration-300',
-                                    isEditMode ? 'opacity-0 scale-0' : 'opacity-100 scale-100',
-                                )}
-                            />
-                            <Save
-                                className={cn(
-                                    'size-5 absolute transition-all duration-300',
-                                    isEditMode ? 'opacity-100 scale-100' : 'opacity-0 scale-0',
-                                )}
-                            />
-                        </Button>
+                        <EditSaveToggle
+                            value={isPromptEditMode}
+                            onEditMode={() => setIsPromptEditMode(true)}
+                            onSaveMode={handlePromptSave}
+                        />
                     </div>
                     <div className="relative group flex-1 min-h-0">
                         <Textarea
                             id="prompt-textarea"
                             className={cn(
                                 'h-full w-full resize-none overflow-y-auto',
-                                isEditMode ? 'cursor-text' : 'cursor-default',
+                                isPromptEditMode ? 'cursor-text' : 'cursor-default',
                             )}
                             value={promptText}
                             onChange={(e) => setPromptText(e.target.value)}
-                            readOnly={!isEditMode}
+                            readOnly={!isPromptEditMode}
                         />
 
                         <CopyButton
                             className={cn(
                                 'size-10 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100',
-                                isEditMode ? 'hidden' : 'block',
+                                isPromptEditMode ? 'hidden' : 'block',
                             )}
                             text={promptText}
                         />
---
+++ packages/renderer/src/components/shared/edit-save-toggle.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Pencil, Save } from "lucide-react";
+
+interface EditSaveToggleProps {
+    className?: string;
+    value?: boolean;
+    onEditMode?: () => void;
+    onSaveMode?: () => void;
+}
+
+export default function EditSaveToggle({ onEditMode, onSaveMode, className, value }: EditSaveToggleProps) {
+
+    function handleEditToggle() {
+        if (value) {
+            onSaveMode?.();
+        } else {
+            onEditMode?.();
+        }
+    }
+
+    return (
+        <Button
+            variant="ghost"
+            className={cn("size-10 sm:size-8", className)}
+            onClick={handleEditToggle}
+        >
+            <Pencil
+                className={cn(
+                    'size-5 absolute transition-all duration-300',
+                    value ? 'opacity-0 scale-0' : 'opacity-100 scale-100',
+                )}
+            />
+            <Save
+                className={cn(
+                    'size-5 absolute transition-all duration-300',
+                    value ? 'opacity-100 scale-100' : 'opacity-0 scale-0',
+                )}
+            />
+        </Button>
+    );
+}
