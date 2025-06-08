5d97135eb601562eba96178762242509fd68e8b8
r3dsd <dogsheepduck@gmail.com>
2025-06-09 03:16:39 +0900

    fix: import 수정

--- packages/main/src/modules/DatabaseModule.ts
+++ packages/main/src/modules/DatabaseModule.ts
@@ -2,7 +2,7 @@ import { sql } from 'kysely';
 import { AppModule } from 'src/AppModule.js';
 import { ModuleContext } from 'src/ModuleContext.js';
 import { db } from '../database/db.js';
-import { isDevMode } from 'src/utils/env.js';
+import { isDevMode } from '../utils/env.js';
 
 class DatabaseModule implements AppModule {
     async enable(context: ModuleContext): Promise<void> {
