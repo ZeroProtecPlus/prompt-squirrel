12b365165b61cf1e21113f96340664d5ee4c4978
r3dsd <dogsheepduck@gmail.com>
2025-06-09 18:37:57 +0900

    fix: 에러 핸들링 추가

--- packages/renderer/src/components/shared/app-menu/import-menu-item.tsx
+++ packages/renderer/src/components/shared/app-menu/import-menu-item.tsx
@@ -1,14 +1,22 @@
 import { importCommand } from '@/commands/menu';
 import { useLoading } from '@/components/hooks';
 import { MenubarItem } from '@/components/ui/menubar';
+import { isServiceException } from '@/lib/utils';
+import { toast } from 'sonner';
 
 export default function ImportMenuItem() {
     const { loading, stopLoading } = useLoading();
 
     async function handleImport() {
-        loading('프롬프트 불러오는 중...');
-        await importCommand();
-        stopLoading();
+        try {
+            loading('프롬프트 불러오는 중...');
+            await importCommand();
+            stopLoading();
+        } catch (error) {
+            if (isServiceException(error)) toast.error('프롬프트를 불러오는 도중 오류가 발생했습니다.');
+
+            stopLoading();
+        }
     }
 
     return <MenubarItem onClick={handleImport}>불러오기</MenubarItem>;
